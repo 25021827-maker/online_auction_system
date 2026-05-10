22352b7c29a929e45c45d9f329fce103a8c5c3b8
Hanshi402 <25021975@vnu.edu.vn>
2026-05-11 03:22:44 +0700

    Up lên branch lần đầu

--- backend/src/main/java/dao/DatabaseConnection.java
+++ backend/src/main/java/dao/DatabaseConnection.java
@@ -8,7 +8,7 @@ public class DatabaseConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/auction_db?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "root";
+    private static final String PASSWORD = "Hanshi@402";
 
 
     private static DatabaseConnection instance;
